8e088932a874dc28f9e2bbee83b4059ef595e7ec
gubi95 <rafalgubala01@gmail.com>
2025-12-30 14:49:10 +0100

    Simple retry mechanism

--- CustomComputationExpression/CustomComputationExpression/AsyncRetryBuilder.fs
+++ CustomComputationExpression/CustomComputationExpression/AsyncRetryBuilder.fs
@@ -3,13 +3,38 @@ module CustomComputationExpression
 open System
 
 type RetryState<'T, 'Error> =
-    { Action: unit -> Result<'T, 'Error> Async
+    { Action: unit -> Result<unit, 'Error> Async
       Retries: TimeSpan list }
+
+let private runWithRetries<'T, 'Error>
+    (action: unit -> Result<'T, 'Error> Async)
+    (retries: TimeSpan list)
+    : Result<'T, 'Error> Async =
+    let rec attempt (retriesLeft: TimeSpan list) =
+        async {
+            printfn "Attempting execution..."
+            let! result = action ()
+
+            match result with
+            | Ok data -> return Ok data
+            | Error e ->
+                match retriesLeft with
+                | [] -> return Error e
+                | waitTime :: rest ->
+                    do! Async.Sleep(int waitTime.TotalMilliseconds)
+                    return! attempt rest
+        }
+
+    attempt retries
+
+module AsyncRetry =
+    let run (state: RetryState<_, _>) =
+        runWithRetries state.Action state.Retries
 
 type AsyncRetryBuilder() =
     member _.Return(value) = async { return Ok value }
 
-    member _.Bind(action, f) =
+    member _.Bind(action: Result<_, _> Async, f) =
         async {
             let! result = action
 
@@ -18,40 +43,37 @@ type AsyncRetryBuilder() =
             | Error e -> return Error e
         }
 
-    member _.MergeSources(action1, action2) =
-        async {
-            let! result1 = action1
-
-            match result1 with
-            | Error e -> return Error e
-            | Ok data1 ->
-                let! result2 = action2
-
-                match result2 with
-                | Error e -> return Error e
-                | Ok data2 -> return Ok(data1, data2)
-        }
-
-    member _.Zero() =
-        { Action = fun () -> async { return Ok() }
-          Retries = List.empty }
-
-    member _.Yield(value) =
-        { Action = fun () -> async { return Ok value }
-          Retries = List.empty }
-
     member _.For(state, f) =
-        async {
-            let! result = state.Action()
+        let newAction =
+            fun () ->
+                async {
+                    let! result = state.Action()
 
-            match result with
-            | Ok data -> return! f data
-            | Error e -> return Error e
-        }
+                    match result with
+                    | Ok _ -> return! f ()
+                    | Error e -> return Error e
+                }
 
-    [<CustomOperation("withRetry", MaintainsVariableSpace = true)>]
-    member _.WithRetry(state, value: TimeSpan) =
+        { state with Action = newAction }
+
+    member _.Yield(()) = ()
+
+    [<CustomOperation("withRetries")>]
+    member _.WithRetries((), value: TimeSpan list) =
+        { Action = (fun () -> async { return Ok() })
+          Retries = value }
+
+    [<CustomOperation("withAction")>]
+    member _.WithAction(state: RetryState<'T, 'Error>, newAction: unit -> Result<unit, 'Error> Async) =
         { state with
-            Retries = state.Retries @ [ value ] }
+            Action =
+                fun () ->
+                    async {
+                        let! result = state.Action()
+
+                        match result with
+                        | Ok _ -> return! newAction ()
+                        | Error e -> return Error e
+                    } }
 
 let asyncRetry = AsyncRetryBuilder()
--- CustomComputationExpression/CustomComputationExpression/Program.fs
+++ CustomComputationExpression/CustomComputationExpression/Program.fs
@@ -1,15 +1,50 @@
 ﻿open System
 open CustomComputationExpression
 
-let main () =
-    asyncRetry {
-        withRetry (TimeSpan.FromSeconds 1.0)
-        withRetry (TimeSpan.FromSeconds 2.0)
-        withRetry (TimeSpan.FromSeconds 4.0)
+let mutable failAction2 = true
 
-        let! result = async { return Ok 10 }
-
-        return result + 5
+let asyncAction1 () : Result<unit, string> Async =
+    async {
+        printfn "Attempting action 1..."
+        return Ok()
     }
 
-main () |> Async.RunSynchronously |> printfn "Result: %A"
+let asyncAction2 () : Result<unit, string> Async =
+    async {
+        printfn "Attempting action 2..."
+
+        return
+            match failAction2 with
+            | true ->
+                failAction2 <- false
+                printfn "Action 2 failed."
+                Error "Action 2 failed"
+            | false -> Ok()
+    }
+
+let main () =
+    asyncRetry {
+        withRetries
+            [ (TimeSpan.FromSeconds 1.0)
+              (TimeSpan.FromSeconds 2.0)
+              (TimeSpan.FromSeconds 4.0) ]
+
+        withAction asyncAction1
+        withAction asyncAction2
+
+        let! _ =
+            async {
+                printfn "Attempting inline action 1..."
+                return Ok 5
+            }
+
+        let! _ =
+            asyncRetry {
+                printfn "Attempting inline action 2..."
+                return Ok 10
+            }
+
+        return ()
+    }
+
+main () |> AsyncRetry.run |> Async.RunSynchronously |> printfn "Result: %A"
